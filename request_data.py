dca02e7de418319241d96a2dbe6126c521207025
Jimmy Ho <ho@jimmyho.com>
2016-06-16 22:18:20 +0100

    Request tickers+TS from Quandl

--- request_data.py
+++ request_data.py
@@ -9,8 +9,8 @@ import pandas as pd
 import quandl
 
 EXCHANGES = [
-    # ('FTSE', 'https://s3.amazonaws.com/static.quandl.com/tickers/FTSE100.csv',),
-    ('SP500', 'https://s3.amazonaws.com/static.quandl.com/tickers/SP500.csv',),
+    ('FTSE', 'https://s3.amazonaws.com/static.quandl.com/tickers/FTSE100.csv',),
+    # ('SP500', 'https://s3.amazonaws.com/static.quandl.com/tickers/SP500.csv',),
     # ('NASDAQ', 'https://s3.amazonaws.com/static.quandl.com/tickers/NASDAQComposite.csv',),
 ]
 
